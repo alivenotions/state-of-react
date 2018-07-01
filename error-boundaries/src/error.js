d3a099f7c088de590f7f3fc6837f3ab0cfb6f68c
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-07-01 19:22:44 +0530

    Broken component

--- error-boundaries/src/error.js
+++ error-boundaries/src/error.js
@@ -1,1 +1,32 @@
-ReactDOM.render(<div>Hi</div>, document.getElementById('root'))
+class Child extends React.Component {
+  constructor () {
+    super()
+    throw new Error('Such Error! Much Breakage! Wow!')
+  }
+  render () {
+    return (
+      <div>
+        <h3>Child</h3>
+      </div>
+    )
+  }
+}
+
+const Parent = () => (
+  <div>
+    <h1>Parent</h1>
+    <Child />
+  </div>
+)
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='App'>      
+        <Parent />
+      </div>    
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
